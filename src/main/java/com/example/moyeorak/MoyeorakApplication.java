7b354fa38a1d7184036a43bff3eeb4f6f99b35bc
choisangmin_git <choisangmin1112@gmail.com>
2025-07-21 09:54:08 +0900

    DOCS git 오류 수정2

--- src/main/java/com/example/moyeorak/MoyeorakApplication.java
+++ src/main/java/com/example/moyeorak/MoyeorakApplication.java
@@ -2,7 +2,9 @@ package com.example.moyeorak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // ✅ 이게 꼭 필요!
 @SpringBootApplication
 public class MoyeorakApplication {
 
