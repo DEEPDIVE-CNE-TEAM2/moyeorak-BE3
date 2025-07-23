5f847a70263263ec7d7de2819396adc7e816fdd8
choisangmin_git <choisangmin1112@gmail.com>
2025-07-23 13:58:13 +0900

    refactor: enrollment 요청을 programId 기반으로 변경 (refs #5)

--- src/main/java/com/example/moyeorak/dto/EnrollmentRequest.java
+++ src/main/java/com/example/moyeorak/dto/EnrollmentRequest.java
@@ -11,17 +11,8 @@ import lombok.*;
 @Builder
 public class EnrollmentRequest {
 
-    @NotBlank(message = "프로그램 제목은 필수입니다.")
-    private String programTitle;
-
-    @NotBlank(message = "시설 위치는 필수입니다.")
-    private String location;
-
-    @NotBlank(message = "이용 기간은 필수입니다. 예: '2025-08-01 ~ 2025-08-31'")
-    private String usagePeriod;
-
-    @NotBlank(message = "수업 시간은 필수입니다. 예: '09:00 ~ 11:00'")
-    private String classTime;
+    @NotNull(message = "프로그램 ID는 필수입니다.")
+    private Long programId;
 
     @NotNull(message = "수강료는 필수입니다.")
     private Integer paidAmount;
